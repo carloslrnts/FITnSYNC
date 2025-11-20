10e4fbf306b81c335116ae0cf3f64080a21e88d5
palnaz <sierra7seven@gmail.com>
2025-11-20 12:52:14 +0700

    feat(personal trainer): added personal trainer page

---
+++ js/personal-trainer.js
@@ -0,0 +1,12 @@
+// FITnSYNC – Personal Trainer Page
+
+// Ambil user
+const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+// Redirect kalau belum login
+if (!currentUser) {
+  window.location.href = "../login.html";
+}
+
+// Tampilkan nama user di navbar
+document.getElementById("profileName").textContent = currentUser.username;
